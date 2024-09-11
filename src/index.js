872cd84717140814eeb95b39f1aee50549b08922
ShubhamShanker510 <78153738+ShubhamShanker510@users.noreply.github.com>
2024-09-11 13:13:07 +0530

    Handling api error and response and using cors, cookie-parser in app.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,20 @@ dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Errror", error);
+        throw error;
+    })
+
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed !!! ",err)
+})
 
 // import express from 'express'
 
